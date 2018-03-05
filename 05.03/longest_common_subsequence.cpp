97ab769aa304a600fcd876fe663baa5f82e7ec6e
ayush1999 <ayush.shridhar1999@gmail.com>
2018-03-05 15:59:41 +0530

    05.03 added

---
+++ 05.03/longest_common_subsequence.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int max(int a, int b)
+{
+    return (a > b) ? a : b;
+}
+
+int lcs( string X, string Y, int m, int n )
+{
+   if (m == 0 || n == 0)
+     return 0;
+   if (X[m-1] == Y[n-1])
+     return 1 + lcs(X, Y, m-1, n-1);
+   else
+     return max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n));
+}
+ 
+int main()
+{
+  string X = "AGGTAB";
+  string Y = "GXTXAYB";
+ 
+  int m = X.length();
+  int n = Y.length();
+ 
+  cout<<"Length of LCS is "<<lcs( X, Y, m, n );
+ 
+  return 0;
+}
